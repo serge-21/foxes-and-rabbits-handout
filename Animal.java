420fdf07fa1213a7a511249f6ea3d52968790a22
fox-lol <tottytwo"gnail.com>
2022-02-08 12:43:45 +0000

    fox is now cool

--- Animal.java
+++ Animal.java
@@ -2,7 +2,7 @@ import java.util.List;
 
 /**
  * A class representing shared characteristics of animals.
- * 
+ * fox is cool
  * @author David J. Barnes and Michael Kölling
  * @version 2016.02.29 (2)
  */
