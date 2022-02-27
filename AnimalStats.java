38eaa2cc55c7aa82376582ce72decea4f839664a
Serge <syraj.alkhalil7@gmail.com>
2022-02-27 15:14:37 +0000

    documentation finished

--- AnimalStats.java
+++ AnimalStats.java
@@ -3,19 +3,17 @@ import java.awt.*;
 /**
  * A container for all the unique values for an animal. It also makes a backup of the values if the originals want to be reassigned if they're changed
  *
- * @author -
- * @version -
+ * @author Syraj Alkhalil and Cosmo Colman
+ * @version 2022.02.27 (2)
  */
 public class AnimalStats extends EntityStats {
-    // Characteristics shared by all predator1s (class variables).
+    // Characteristics shared by all Animals
     private int breedingAge;                 // The age at which a predator1 can start to breed.
-    private int maxAge;                     // The age to which a predator1 can live.
-    //private double breedingProbability;    // The likelihood of a predator1 breeding.
+    private int maxAge;                      // The age to which a predator1 can live.
     private int maxLitterSize;               // The maximum number of births.
-    private int hungerValue;             // The food value of a single prey1. In effect, this is the
-    // number of steps a predator1 can go before it has to eat again.
+    private int hungerValue;                 // The food value of a single prey1. In effect, this is the
 
-    private final AnimalStats DEFAULT_STATS;
+    private final AnimalStats DEFAULT_STATS; // The default stats of the animal
 
     /**
      * Created a container for the animal's statistics.
@@ -43,7 +41,7 @@ public class AnimalStats extends EntityStats {
      * Clones a pre-existing AnimalStats.
      * @param clone The AnimalStats you wish to clone.
      */
-    protected AnimalStats(AnimalStats clone){
+    protected AnimalStats(AnimalStats clone) {
         super(clone);
         this.breedingAge = clone.breedingAge;
         this.maxAge = clone.maxAge;
@@ -56,7 +54,7 @@ public class AnimalStats extends EntityStats {
     /**
      * Creates an AnimalStats with all values set to 0 and type to PREY.
      */
-    public AnimalStats(){
+    public AnimalStats() {
         super();
         this.breedingAge = 0;
         this.maxAge = 0;
@@ -66,37 +64,19 @@ public class AnimalStats extends EntityStats {
         DEFAULT_STATS = new AnimalStats(this);
     }
 
+    /**
+     * A simple getter method to get the default stats of the animal
+     *
+     * @return the default stats field
+     */
     @Override
-    public AnimalStats getDefaults(){
+    public AnimalStats getDefaults() {
         return DEFAULT_STATS;
     }
 
-    public int getBreedingAge(){
-        return breedingAge;
-    }
-    public int getMaxAge(){
-        return maxAge;
-    }
-    public int getMaxLitterSize(){
-        return maxLitterSize;
-    }
-    public int getHungerValue(){
-        return hungerValue;
-    }
-
-    public void setBreedingAge(Integer breedingAge) {
-        this.breedingAge = breedingAge;
-    }
-    public void setMaxAge(int maxAge) {
-        this.maxAge = maxAge;
-    }
-    public void setMaxLitterSize(int maxLitterSize) {
-        this.maxLitterSize = maxLitterSize;
-    }
-    public void setHungerValue(int hungerValue) {
-        this.hungerValue = hungerValue;
-    }
-
+    /**
+     * resets the stats to the default
+     */
     @Override
     public void resetToDefault() {
         super.resetToDefault();
@@ -110,5 +90,80 @@ public class AnimalStats extends EntityStats {
         this.maxAge = defaults.maxAge;
         this.maxLitterSize = defaults.maxLitterSize;
         this.hungerValue = defaults.hungerValue;;
+    }
+
+    // a list of all the getter and setter methods
+
+    /**
+     * A simple getter method to return the breeding age
+     *
+     * @return the breedingAge field
+     */
+    public int getBreedingAge() {
+        return breedingAge;
+    }
+
+    /**
+     * A simple getter method to return the max age
+     *
+     * @return the maxAge field
+     */
+    public int getMaxAge() {
+        return maxAge;
+    }
+
+    /**
+     * A simple getter method to return the max litter for the animal
+     *
+     * @return the maxLitterSize field
+     */
+    public int getMaxLitterSize() {
+        return maxLitterSize;
+    }
+
+    /**
+     * A simple getter method to return the hunger value of the animal
+     *
+     * @return the hungerValue field
+     */
+    public int getHungerValue(){
+        return hungerValue;
+    }
+
+    /**
+     * A simple setter method to set the breeding age
+     *
+     * @param breedingAge the age we want the breeding to occur
+     */
+    public void setBreedingAge(Integer breedingAge) {
+        this.breedingAge = breedingAge;
+    }
+
+    /**
+     * A simple setter method to set the max age (Age at which the animal dies)
+     *
+     * @param maxAge the max age we want the animal to live for
+     */
+    public void setMaxAge(int maxAge) {
+        this.maxAge = maxAge;
+    }
+
+    /**
+     * A simple setter method to set the max litter size (max number of offspring per animal)
+     *
+     * @param maxLitterSize the max offspring per type of animal
+     */
+    public void setMaxLitterSize(int maxLitterSize) {
+        this.maxLitterSize = maxLitterSize;
+    }
+
+    /**
+     * A simple setter method to set the hunger value this is used before/after eating
+     * or if the animal doesn't eat at all
+     *
+     * @param hungerValue the food value of the animal consumed
+     */
+    public void setHungerValue(int hungerValue) {
+        this.hungerValue = hungerValue;
     }
 }
