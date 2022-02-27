b7b95d7051f25bffdea86559d6976753c26d347e
Serge <syraj.alkhalil7@gmail.com>
2022-02-27 16:47:11 +0000

    documentation finished

--- Organism.java
+++ Organism.java
@@ -1,18 +1,24 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The Organism class is responsible for distinguishing what can and can't be infected
+ *
+ * @author Syraj Alkhalil and Cosmo Colman
+ * @version 2022.02.27 (2)
+ */
 public abstract class Organism extends Entity {
-    private int moveSpeed;                                      // current movement speed
-    private boolean isInfected;                                         // to be passed in
-    private ArrayList<Disease> diseases;
+    private boolean isInfected;                                 // Is this organism infected
+    private final ArrayList<Disease> diseases;                  // the list of disease this organism has
 
     /**
      * Create an Organism. An organism can be created with a field and
      * a location.
+     * @param stats the statistics of the organism
      * @param field The field currently occupied.
      * @param initLocation The location within the field.
      */
-    public Organism(EntityStats stats, Field field, Location initLocation){
+    public Organism(EntityStats stats, Field field, Location initLocation) {
         super(stats, field, initLocation);
         diseases = new ArrayList<>();
         this.isInfected = false;
@@ -21,41 +27,46 @@ public abstract class Organism extends Entity {
     /**
      * @param newOrganisms A list to return newly born animals.
      * @param isDay is it currently day or night ?
+     * @param currentWeather the current weather
      */
     abstract public void act(List<Organism> newOrganisms, boolean isDay, Weather currentWeather);
 
-    // either two different act methods or just deal with the code being repeated.
-
-    public void addDisease(Disease disease){
+    /**
+     * make this organism infected with the disease
+     *
+     * @param disease the disease we wish this organism to be infected by
+     */
+    public void addDisease(Disease disease) {
         this.diseases.add(disease);
     }
 
+    /**
+     * A simple getter method that return isInfected field
+     *
+     * @return is this organism ifected
+     */
     public boolean getIsInfected() {
         return isInfected;
     }
-    public void setInfected(boolean infection){
+
+    /**
+     * A simple setter field that sets the isInfected field
+     *
+     * @param infection true if this organism is infected false otherwise
+     */
+    public void setInfected(boolean infection) {
         this.isInfected = infection;
     }
 
-    public void setMoveSpeed(int moveSpeed){
-        this.moveSpeed = moveSpeed;
-    }
-    public int getMoveSpeed(){
-        return moveSpeed;
-    }
-
-
-    // this could be probably a bit more efficient but i will leave it for now.
-    protected void dieDueToInfection(){
+    /**
+     * let the current organism die due to infection
+     * balancing this is a nightmare
+     */
+    protected void dieDueToInfection() {
         for(Disease disease : this.diseases){
             if(isInfected && getRand().nextDouble() < disease.getDeadliness()){
                 setDead();
             }
         }
     }
-
-    // TODO this entire class has no point now.
-    // TODO move class Organism into entity.
-    // TODO ORRRRRRRR keep it for easier distinction ???
-    // TODO DECIDE IN A WEEK FROM NOW.
 }
